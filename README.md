8ed3297b5a1d2d6d091ff5c5c9af948ad6ae3805
James Wyn <thisisjameswyn@gmail.com>
2021-04-25 10:51:02 -0700

    finalize project rebuild and functionality[B

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# PROJECT NAME
+## James Wyn
 
-## By thisisjameswyn/James
+# Currency Exchange
 
----
+### Description
 
-### Project Description
+This is my sixth week project for Epicodus. The objective is to take a given currency value and convert to a selected country's value using current data retrieved from https://www.exchangerate-api.com/. The form should allow for the selection of two countries and an amount to exchange, then return the converted value back to the user.
 
-## This project is a web application that DESCRIPTION
+This program requires an API key which can be obtained here https://www.exchangerate-api.com/. Once you have your key you can put it in the .env file as described in the instructions.
 
 ### Tech Stack
 
@@ -23,18 +23,41 @@
 
 ### Project Build Available At:
 
-[BUILD URL](BUILD URL)
+https://github.com/Thisisjameswyn/currency_project
 
 ---
 
-### Installation Instructions
+## Setup/Installation Requirements
 
-1. Clone this repo: `git clone https://github.com/thisisjameswyn/REPO URL`
-2. Enter new directory `cd REPO DIR`
-3. Install dependencies with `npm install`
-4. To run it locally, use the npm dev command `npm run start:dev`
-5. Open your web browser and navigate to http://localhost:9001
-6. For a production build, use `npm run build`
+### Without Github template clone:
+
+- Clone Repo
+- Delete .git if needing a clean commit history
+- Initilize git
+- Add remote repository to new git
+- Create a file called .env in the top level of the directory
+- Make first commit only adding .gitignore to commit
+- Inside the .env file place the following line of code: API_KEY=[Your API key goes here without brackets]
+- Open new repo to begin npm initilization
+
+### With Github
+
+- Press use this template
+- Create new repo
+- Clone new repo down to
+- Open new repo to begin npm initilization
+
+### Npm Initilization
+
+- Verify package.json has correct npm packages for needs
+- Modify title on line 18 of webpack.config.js
+- Modify title on line 2 of package.json
+- Modify description on line 19 of package.json
+- Modify title on line 2 if package-lock.json
+- Run `npm init -y`
+- Run `npm install`
+- Verify install works by running `npm run build`
+- Run the application by typing `npm run start:dev`
 
 ---
 
@@ -45,18 +68,9 @@
 
 ---
 
-### Test Suite/Specification
-
-> See also [the Jest Test File](TESTSURL) for Jest Test Suite which includes 14 passing tests. View Test Results from Project Directory by Running `yarn run test` or if you prefer npm `npm run test`
-
-```
-Describe: TESTS
-
-```
-
----
-
 ### License is GPLv3 and I make no claim to copyright.
+
+&copy; James Wyn 2021
 
 <br />
 
--- src/main.js
+++ src/main.js
@@ -4,35 +4,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
 import ExchangeReturn from "./js/exchangeService";
 
-// function clearFields() {
-//   $("#amount").val("");
-// }
+function clearFields() {
+  $("#convertedAmount").val("");
+}
 
-// function getElements(response) {
-//   if (response.result === "success") {
-//     $(".showConv").text(
-//       `The conversion rate from ${} to ${} is : ${
-//         response.conversion_rate
-//       } for a total value of ${(
-//         _amount * response.conversion_rate
-//       ).toLocaleString("en", { style: "currency", currency: outPut })}`
-//     );
-//   } else if (response["error-type"] === "unsupported-code") {
-//     $(".showErrors").text("Sorry that type of currency is not supported");
-//   } else if (response.result === "error") {
-//     $(".showErrors").text(
-//       `Uh-oh, something went wrong: ${response["error-type"]}`
-//     );
-//   } else {
-//     $(".showErrors").text(`Uh-oh, something went wrong: ${response.message}`);
-//   }
-// }
+function calcExchange(rateFrom, rateTo, amountFrom) {
+  let curInUSD = amountFrom / rateFrom;
+  let convertedAmount = curInUSD * rateTo;
+  return convertedAmount;
+}
 
 $(document).ready(function () {
   ExchangeReturn.getExRate().then(function (response) {
     if (response.result === "success") {
-      const body = JSON.parse(response);
-      const conversionRates = Object.entries(body.conversion_rates);
+      const conversionRates = Object.entries(response.conversion_rates);
       for (let i = 0; i < conversionRates.length; i++) {
         $("#currencyOne").append(
           $("<option>", {
@@ -48,29 +33,37 @@ $(document).ready(function () {
         );
       }
     } else if (response["error-type"] === "unsupported-code") {
-      $(".showErrors").text("Sorry that type of currency is not supported");
+      $("#showErrors").text("Sorry that type of currency is not supported");
     } else if (response.result === "error") {
-      $(".showErrors").text(
+      $("#showErrors").text(
         `Uh-oh, something went wrong: ${response["error-type"]}`
       );
     } else {
-      $(".showErrors").text(`Uh-oh, something went wrong: ${response.message}`);
+      $("#showErrors").text(`Uh-oh, something went wrong: ${response.message}`);
     }
   });
 
-  $("#search").click(function () {
-    let amount = parseInt($("#amount").val());
-    // const curOneText = $("#currencyOne option:selected").text();
-    // const curTwoText = $("#currencyTwo option:selected").text();
-    // const curOne = $("#currencyOne").val();
-    // const curTwo = $("#currencyTwo").val();
+  $("#converterSubmit").submit(function (event) {
+    event.preventDefault();
+    let amount = parseInt($("#convertedAmount").val());
+    const curOneText = $("#currencyOne option:selected").text();
+    const curTwoText = $("#currencyTwo option:selected").text();
+    const curOne = $("#currencyOne").val();
+    const curTwo = $("#currencyTwo").val();
     if (isNaN(amount)) {
       $(alert("please enter an amount"));
       location.reload();
     }
-    // clearFields();
-    // ExchangeReturn.getExRate(curOne, curTwo).then(function (response) {
-    //   getElements(response, curOneText, curTwoText, amount, curTwo);
-    // });
+    clearFields();
+    let convertedAmount = calcExchange(curOne, curTwo, amount);
+    $("#output").text(
+      `The conversion from ${curOneText} to ${curTwoText} is : ${convertedAmount.toLocaleString(
+        "en",
+        {
+          style: "currency",
+          currency: curTwoText,
+        }
+      )}`
+    );
   });
 });
